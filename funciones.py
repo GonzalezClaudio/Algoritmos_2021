c395467ded042b055313ac8178e374b3ee6553bc
Gonzalez Claudio <claudioggonzalez3@gmail.com>
2021-04-03 18:34:05 -0300

    nuevos archivos

---
+++ funciones.py
@@ -0,0 +1,8 @@
+numero=(input('ingrese un numero'))
+if (numero > 0):
+    print('es positivo')
+elif (numero<0):
+    print('es negativo')
+else:
+    print('es neutro')
+print('fin del codigo')
---
+++ inicio.py
@@ -0,0 +1,20 @@
+
+num1 = int(input('ingrese su numero'))
+
+#if (numero > 0):
+ #   print('es positivo')
+#elif (numero<0):
+#    print('es negativo')
+#else:
+#    print('es neutro')
+#print('fin del codigo')
+
+# funciones
+def suma(num1,num2):
+    print('la suma es:' , num1 + num2)
+
+suma(num1 , 5)
+
+
+
+
